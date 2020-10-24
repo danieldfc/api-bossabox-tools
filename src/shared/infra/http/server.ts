af1d794eef5ec83d66ef54e59631d2782609545a
Daniel Felizardo <daniel.david772@gmail.com>
2020-10-24 15:04:10 -0300

    config: production

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,9 @@
 import app from './app';
 
+if (process.env.NODE_ENV === 'production') {
+  require('module-alias/register');
+}
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('🚀 Sever started on port 3000!');
 });
